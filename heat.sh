175c0caf9ea21dffba78ce0b1f4fd9f149f9019a
Tomas Pospisek <tpo_hp@sourcepole.ch>
2013-05-19 14:37:44 +0200

    unify --help usage

--- heat.sh
+++ heat.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+help() {
+   echo 'usage: heat.sh'
+   echo
+   echo 'run infinite loop that will consume 100% of a CPU core'
+   echo
+   echo 'Useful for fan and temperature regulation testing'
+   echo
+   exit 1
+}
+
+[ "$1" == "--help" ] && help
+
 while true; do
 	i=$(( $i + 1 ))
 done
